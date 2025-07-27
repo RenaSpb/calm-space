2378a8b88b24daec2e57013addc662bf9ca496e0
Renata <murzinarenata@gmail.com>
2025-07-27 12:28:18 -0700

    css

--- client/src/Components/Videos/VideosPage.jsx
+++ client/src/Components/Videos/VideosPage.jsx
@@ -4,43 +4,43 @@ export default function VideosPage() {
   const videos = [
     {
       id: "SMgb9LJmkWc",
-      title: "1 Hour of Gentle Ambient & Classical Lullabies",
+      title: "Gentle Ambient",
     },
     {
       id: "T-BH-Pk7YwQ",
-      title: "5 Minutes of Peace for Meditation",
+      title: "Peace for Meditation",
     },
     {
       id: "Cf2NGcHXoK4",
-      title: "2 Hours of Rain and Thunderstorm Sounds",
+      title: "Thunderstorm Sounds",
     },
     {
       id: "9Qwp89Zw8HU",
-      title: "Relaxing Piano & Cozy Bedroom Ambience",
+      title: "Relaxing Piano",
     },
     {
       id: "HZKI6UQF8nQ",
-      title: "10 Hours of White Noise for Sleep, Focus, and Calm",
+      title: "White Noise for Sleep",
     },
     {
       id: "q18asXPDLEo",
-      title: "1 Hour of Chill Ambience",
+      title: "Chill Ambience",
     },
     {
       id: "Pk809gbPMdw",
-      title: "1 Hour Calm, Meditation, Relaxation Music",
+      title: "Relaxation Music",
     },
     {
       id: "c5X2P7GNUD4",
-      title: "30 Minutes of Forest Ambience with Birdsong",
+      title: "Forest Ambience",
     },
     {
       id: "uVbIX7B6BsA",
-      title: "2 Hours of Soothing Fireplace Sounds",
+      title: "Fireplace Sounds",
     },
     {
       id: "J4d-a7dVtiQ",
-      title: "8 Hours of Uninterrupted Rain Ambience",
+      title: "Rain Ambience",
     },
   ];
 
